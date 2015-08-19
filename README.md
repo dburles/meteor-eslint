874fa782d159da24fc5c2c4b854ea1427752fe37
David Burles <dburles@gmail.com>
2015-08-19 15:14:38 +1000

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Meteor Eslint
+
+The pluggable linting utility for JavaScript and JSX. Adapted from the core jshint package.
+
+## Installation
+
+```sh
+$ meteor add dburles:eslint
+```
+
+## Usage
+
+Add an `.eslintrc` file to your project root. Here's a slightly modifed version of the Meteor configuration https://gist.github.com/dburles/a2f7ea77189b268b660d
+
+### License
+
+MIT
---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'dburles:eslint',
+  version: '1.0.0',
+  summary: 'The pluggable linting utility for JavaScript and JSX.',
+  documentation: 'README.md',
+  git: 'https://github.com/dburles/meteor-eslint.git'
+});
+
+Package.registerBuildPlugin({
+  name: "eslint",
+  sources: [
+    'plugin/eslint.js'
+  ],
+  npmDependencies: {
+    "eslint": "1.1.0",
+    "strip-json-comments": "1.0.4"
+  }
+});
+
+Package.onUse(function(api) {
+  api.use('isobuild:linter-plugin@1.0.0');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('dburles:eslint');
+});
